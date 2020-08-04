61475c56f0fccd481f7982a6452cbc1bf2baa26c
hahm-d <dhahm01@gmail.com>
2020-08-04 15:58:22 -0400

    ham

---
+++ src/task.js
@@ -0,0 +1,13 @@
+class Task {
+    constructor(){
+        this.task = [];
+    }
+
+    //method to add description 
+
+//invoked functions to render list
+    
+//invoked function to delete from list 
+
+
+}

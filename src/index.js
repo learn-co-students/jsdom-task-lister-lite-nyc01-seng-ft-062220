dcd794d0a54c3fd2b269a7b4daa05d9960f27cc1
Aiannn <soktoevayan@gmail.com>
2020-08-04 18:13:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const newTaskForm = document.getElementById("create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description")
+  //const newTaskPriority = document.querySelector('input [type="submit"]')
+
+  const newTaskUl = document.getElementById("tasks")
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById("new-task-description")
+  const newTask = document.createElement("li");
+  const delButton = document.createElement("button")
+  newTask.innerText = newTaskDescription.value 
+
+  appendNewTask(newTask);
+  event.target.reset();
+
+  document.getElementsByTagName('li').append(delButton)
+  //deleteButton(delButton);
+  //event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+}
+
+//const deleteButton = task => {
+//  document.getElementsByTagName('li').appendChild(task)
+//}
+
+
+
+
+
+

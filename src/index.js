d45059c8077443622724e8be13173edb5bc47d2b
github username <github email address>
2020-08-05 07:08:48 -0400

    transfer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+// OO solution
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    /*
+    1. add listener (submit) to the form
+    2. get text out of the form.
+    3. create li
+    4. post li to ul.
+     */
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault()
+        const todo = e.target['new-task-description'].value;
+        const toDoLi = document.createElement('li');
+        toDoLi.textContent = todo;
+        const button = document.createElement("button")
+        button.textContent = "X";
+        toDoLi.append(button)
+
+        button.addEventListener('click', function (e) {
+
+            const taskli = e.target.parentElement
+            taskli.remove()
+
+        })
+
+
+        let task = document.getElementById('tasks')
+
+        task.append(toDoLi)
+
+
+
+    })
+
+
+
+
 });
